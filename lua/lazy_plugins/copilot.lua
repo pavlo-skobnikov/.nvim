3ab3d46bd4fd2f89f6054f3d5759c660048c3e66
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-04-23 18:50:43 +0300

    Remove copilot custom accept mapping

--- lua/lazy_plugins/copilot.lua
+++ lua/lazy_plugins/copilot.lua
@@ -4,9 +4,5 @@ return {
   config = function()
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_filetypes = { ['TelescopePrompt'] = false }
-
-    U.register_keys({
-      ['<c-s-y>'] = { 'copilot#Accept("\\<CR>")', 'Accept Copilot suggestion' },
-    }, { silent = true, expr = true, script = true, replace_keycodes = false, mode = 'i' })
   end,
 }
